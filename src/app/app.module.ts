81aaa39aa7688228a05b733100505762ff9a2111
Akshay Khairnar <khairnaraksh@gmail.com>
2018-04-25 12:48:52 +0530

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+ import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import{DataService} from './data.service';
+
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
+  ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+
+})
+export class AppModule { }
